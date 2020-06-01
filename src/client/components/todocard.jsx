c030e11166842f8c92cad033e42faa9225f0f00b
Dyana Wu <dyanawu@gmail.com>
2020-06-02 03:07:42 +0800

    Restyle todo card

--- src/client/components/todocard.jsx
+++ src/client/components/todocard.jsx
@@ -7,16 +7,27 @@ class TodoCard extends Component {
   }
 
   render() {
+    let stamp = this.props.todoStamp;
+    console.log(moment().diff(stamp, 'seconds'));
+    if (moment().diff(stamp, 'seconds') > 6) {
+      console.log("here");
+      stamp = this.props.todoStamp.format("ddd, DD-MM-YYYY, HH:mm");
+    } else {
+      stamp = this.props.todoStamp.fromNow();
+    }
+
     return(
-      <div className="todo-card" data-key={this.props.id}>
-        <span>{this.props.todo}</span>
+      <div className="todo-container">
+        <div className="todo-card">
+        <span className="wrap">{this.props.todo}</span>
         <span><button
                 className="todo-item-button"
                 onClick={(id) => {this.props.todoRemove(this.props.id)}}
 
               >X</button></span>
+        </div>
         <div className="timestamp">
-          {this.props.todoStamp.fromNow()}
+          {stamp}
         </div>
       </div>
     );
